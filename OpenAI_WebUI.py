25aace90a9de4ccd25591df3409f5ad214184578
Martial Michel <mmichel@infotrend.com>
2023-11-20 00:59:42 -0500

    Added information to set_page_config

--- OpenAI_WebUI.py
+++ OpenAI_WebUI.py
@@ -19,7 +19,7 @@ from datetime import datetime
 
 #####
 iti_version="0.9.1"
-st.set_page_config(page_title=f"OpenAI API WebUI ({iti_version})", page_icon="🫥", layout="wide", initial_sidebar_state="expanded")
+st.set_page_config(page_title=f"OpenAI API WebUI ({iti_version})", page_icon="🫥", layout="wide", initial_sidebar_state="expanded", menu_items={'Get Help': 'https://github.com/Infotrend-Inc/OpenAI_WebUI', 'About': "# OpenAI WebUI\n Brought to you by [Infotrend Inc.](https://www.infotrend.com/)"})
 
 
 #####
